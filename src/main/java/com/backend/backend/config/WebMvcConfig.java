de9bbb0b522aded126a72afaebaa0a1da2f7eaac
tharakadasun <126054222+tharakadasun@users.noreply.github.com>
2023-03-30 09:39:21 +0530

    Add files via upload

--- src/main/java/com/backend/backend/config/WebMvcConfig.java
+++ src/main/java/com/backend/backend/config/WebMvcConfig.java
@@ -1,19 +1,29 @@
 package com.backend.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    String url = "https://shopping-center-lime.vercel.app";
-//    String url ="http://localhost:3000";
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins(url)
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+//    @Override
+//    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//        configurer.defaultContentType(MediaType.TEXT_PLAIN);
+//    }
+
+//    String url = "https://shopping-center-lime.vercel.app";
+//    private final String[] allowedOrigins = new String[]{"http://localhost:3000"};
+//    private final String[] allowedOrigins = new String[]{"https://shopping-center-lime.vercel.app"};
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins(allowedOrigins)
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("*")
+////                .exposedHeaders("Authorization")
+//                .allowCredentials(true);
+////                .allowedHeaders("Authorization");
+//    }
 }
